57ccc8d6e55dc53bff62d05cf88b66ff01ddf376
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-09-08 08:54:22 +0530

    Update LoginForm.jsx

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+/* eslint-disable react/no-unescaped-entities */
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
@@ -34,9 +35,9 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-        <h1 className="text-xl font-bold my-4">Login</h1>
+    <div className="grid h-screen place-items-center">
+      <div className="p-5 border-t-4 border-green-400 rounded-lg shadow-lg">
+        <h1 className="my-4 text-xl font-bold">Login</h1>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
@@ -49,16 +50,16 @@ export default function LoginForm() {
             type="password"
             placeholder="Password"
           />
-          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+          <button className="px-6 py-2 font-bold text-white bg-green-600 cursor-pointer">
             Login
           </button>
           {error && (
-            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+            <div className="px-3 py-1 mt-2 text-sm text-white bg-red-500 rounded-md w-fit">
               {error}
             </div>
           )}
 
-          <Link className="text-sm mt-3 text-right" href={"/register"}>
+          <Link className="mt-3 text-sm text-right" href={"/register"}>
             Don't have an account? <span className="underline">Register</span>
           </Link>
         </form>
