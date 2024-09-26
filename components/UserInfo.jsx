b363b3428b0b774ce615efdae58901802dcd0340
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-09-26 14:29:12 +0530

    Update UserInfo.jsx

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -52,7 +52,7 @@ export default function UserInfo() {
 
             <div className="flex justify-center mb-8">
               <button className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Explore</button>
-              <button className="px-4 py-2 text-gray-700 bg-gray-300 rounded-md hover:bg-gray-400">Proceed</button>
+              <button className="px-4 py-2 text-gray-700 bg-gray-300 rounded-md hover:bg-gray-400">proceed</button>
             </div>
 
             <section className="mb-8">
