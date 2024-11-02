7e9e9feb1f9288747fc8ac51fd5b7de4c9535bec
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-11-02 20:45:34 +0530

    co update

--- app/Canteendashboard/Body/Cashier/page.jsx
+++ app/Canteendashboard/Body/Cashier/page.jsx
@@ -4,7 +4,7 @@ const Cashier = () => {
   return (
     <div className="flex p-4 space-x-4">
       <div className="flex items-center justify-center w-full h-32 p-6 text-white bg-gray-700 rounded-md">
-        <div className="text-xl font-bold text-orange-500">Cashier</div>
+        <div className="text-xl font-bold text-orange-500">Cashiers</div>
       </div>
     </div>
   );
