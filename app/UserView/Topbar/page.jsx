725b0a8d34f037cf93ad7bce80b7dc88f57e6800
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-11-07 07:21:49 +0530

    topbar update

--- app/UserView/Topbar/page.jsx
+++ app/UserView/Topbar/page.jsx
@@ -1,13 +1,14 @@
+"use client";
 import { Settings, UserCircle, UserRound } from 'lucide-react';
 import React from 'react';
-import { useSession } from "next-auth/react";
+
 
 const Topbar = () => {
-  const { data: session } = useSession();
+  
 
   return (
     <div className="flex items-center justify-between p-4 bg-white">
-      <span className='text-lg'>Hi {session?.user?.firstName}, Welcome Back</span>
+      <span className='text-lg'>Hi Welcome Back</span>
       <div className="flex space-x-4">
         <span><UserCircle/></span> {/* Profile Icon Placeholder */}
         <span className='text-[#ff842f]'><Settings/></span> {/* Settings Icon Placeholder */}
