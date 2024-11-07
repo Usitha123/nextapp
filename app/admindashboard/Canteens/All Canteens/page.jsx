1e55939e939f007fff009f19dced8c3c710c2d88
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-11-07 07:18:31 +0530

    Update all canteens

--- app/admindashboard/Canteens/All Canteens/page.jsx
+++ app/admindashboard/Canteens/All Canteens/page.jsx
@@ -1,21 +1,7 @@
 import React from 'react'
-import Sidebar from '../Sidebar/page';
-import Topbar from '../Topbar/page';
-import Header from '../Header/page';
 
-function page() {
+export default function page() {
   return (
-    <div className="flex">
-      <Sidebar/>
-      <div className="flex-1">
-        <Topbar />
-        <Header title="All Canteens" />
-        <div className="p-4">
-            <h1>All Canteens</h1>
-        </div>
-      </div>
-    </div>
+    <div>page</div>
   )
 }
-
-export default page
