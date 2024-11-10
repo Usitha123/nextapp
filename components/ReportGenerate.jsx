2564fda5fa01323dfbae5282862e39eb14fe67d3
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-11-10 09:53:27 +0530

    create report generate

---
+++ components/ReportGenerate.jsx
@@ -0,0 +1,40 @@
+"use client"; // Add this line to mark the component as a client component
+
+import html2pdf from 'html2pdf.js';
+import { useRef } from "react";
+import Invoice from "./Invoice";
+
+export default function dashboard() {
+  const contentRef = useRef();
+
+  const exportPDF = () => {
+    const options = {
+      filename: 'canteen-dashboard.pdf',
+      margin: [10, 10, 10, 10],
+      x: 10,
+      y: 10,
+      html2canvas: { scale: 2 },  // Optional: Enhance quality of rendering
+    };
+    html2pdf().from(contentRef.current).set(options).save();
+  };
+
+  return (
+    <div className="flex">
+      <h1>Hello</h1>
+      <div className="flex-1">
+        <h1>Hello usitha</h1>
+
+        <div className="p-4" ref={contentRef}>
+          <Invoice />
+        </div>
+        
+        <button
+          onClick={exportPDF}
+          className="px-4 py-2 text-white bg-blue-500 rounded"
+        >
+          Export as PDF
+        </button>
+      </div>
+    </div>
+  );
+}
