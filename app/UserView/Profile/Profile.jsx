eff84df77a98a5374f0cda2df70be611d09d46a1
Mitharaf <mit79888@gmail.com>
2025-03-02 13:11:36 +0530

    user profile edit

--- app/UserView/Profile/Profile.jsx
+++ app/UserView/Profile/Profile.jsx
@@ -41,44 +41,52 @@ export default function Profile() {
   }
 
   return (
-    <div className="w-full max-w-3xl p-8 mx-auto bg-white rounded-xl">
+    <div className="w-full max-w-md p-8 mx-auto bg-white rounded-xl">
       {/* Profile Icon */}
       <div className="relative flex items-center justify-center w-24 h-24 mx-auto text-orange-600 bg-orange-100 rounded-full">
         <User2Icon className="text-3xl font-bold" />
       </div>
 
-      {/* Information Sections */}
-      <div className="grid grid-cols-2 gap-4 mt-8">
-        <label className="block text-sm text-black">First Name:</label>
-        <span>{targetStudent.firstName || "N/A"}</span>
-      </div>
+      <div className="text-sm space-y-4 m-4">
+        {/* Information Sections */}
       <div className="grid grid-cols-2 gap-4">
-        <label className="block text-sm text-black">Last Name:</label>
-        <span>{targetStudent.lastName || "N/A"}</span>
+        <label className="text-gray-600">First Name</label>
+        <span className="text-right">{targetStudent.firstName || "N/A"}</span>
       </div>
+      <hr/>
       <div className="grid grid-cols-2 gap-4">
-        <label className="block text-sm text-black">Phone:</label>
-        <span>{targetStudent.phoneNumber || "N/A"}</span>
+        <label className="text-gray-600">Last Name</label>
+        <span className="text-right">{targetStudent.lastName || "N/A"}</span>
       </div>
+      <hr/>
       <div className="grid grid-cols-2 gap-4">
-        <label className="block text-sm text-black">Email:</label>
-        <span>{targetStudent.email || "N/A"}</span>
+        <label className="text-gray-600">Phone</label>
+        <span className="text-right">{targetStudent.phoneNumber || "N/A"}</span>
       </div>
+      <hr/>
       <div className="grid grid-cols-2 gap-4">
-        <label className="block text-sm text-black">Faculty:</label>
-        <span>{targetStudent.faculty || "N/A"}</span>
+        <label className="text-gray-600">Email</label>
+        <span className="text-right">{targetStudent.email || "N/A"}</span>
+      </div>
+      <hr/>
+      <div className="grid grid-cols-2 gap-4">
+        <label className="text-gray-600">Faculty</label>
+        <span className="text-right">{targetStudent.faculty || "N/A"}</span>
       </div>
       
 
       {/* Edit Button */}
-      <div className="flex items-start mt-6 space-x-6">
+      <div className="flex">
         <Link
           href={`/UserView/Profile/Edit?id=${targetStudent._id}`}
-          className="px-4 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-400 focus:outline-none"
+          className="font-bold mx-auto mt-4 px-6 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-400 focus:outline-none"
         >
           Edit
         </Link>
       </div>
+      
+       </div>
+      
     </div>
   );
 }
