f659a9f8fe465f398f4d509d9a517896ffe01d72
Mitharaf <mit79888@gmail.com>
2025-03-04 08:34:53 +0530

    admin canteen delete modal

--- app/admindashboard/Canteens/AllCanteens/Deletecanteens.jsx
+++ app/admindashboard/Canteens/AllCanteens/Deletecanteens.jsx
@@ -5,19 +5,21 @@ const Deletecanteens = ({ isOpen, onClose, onDelete }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50">
-      <div className="w-1/3 p-6 bg-white rounded-lg shadow-lg">
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm">
+      <div className="w-1/3 p-6 bg-black text-gray-300 rounded-lg shadow-lg">
         <h2 className="mb-4 text-xl font-semibold text-center">Are you sure you want to delete this canteen?</h2>
         <div className="flex justify-around">
           <button 
             onClick={onDelete}
-            className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-400"
+            className=" px-4 py-2 text-sm font-medium bg-orange-500 text-black rounded-xl hover:bg-orange-600
+"
           >
-            Yes, Delete
+            Yes
           </button>
           <button 
             onClick={onClose}
-            className="px-4 py-2 text-white bg-gray-500 rounded-md hover:bg-gray-400"
+            className=" px-4 py-2 text-sm font-medium bg-[#3B3737] text-orange-500 border border-orange-500 rounded-xl hover:bg-black
+"
           >
             Cancel
           </button>
