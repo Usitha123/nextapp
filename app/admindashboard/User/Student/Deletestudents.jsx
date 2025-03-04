02bb8ca29b3fc4252fb0a7828844e17c4d4959a7
Mitharaf <mit79888@gmail.com>
2025-03-04 09:54:59 +0530

    admin-student modals

--- app/admindashboard/User/Student/Deletestudents.jsx
+++ app/admindashboard/User/Student/Deletestudents.jsx
@@ -4,26 +4,24 @@ const UpdateStatusModal = ({ isOpen, onClose, student, onDelete }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="p-6 text-white bg-gray-800 rounded-lg w-80">
-        <div className="text-center">
-          <h3 className="mb-4 text-lg font-semibold">Are you sure you want to delete this student?</h3>
-        </div>
-        <div className="flex justify-end mt-6 space-x-4">
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm">
+      <div className="p-6 bg-black text-gray-300 rounded-lg shadow-lg">
+        <h3 className="mb-6 font-semibold text-center">
+          Are you sure you want to delete this student?
+        </h3>
+        <div className="flex justify-around">
           <button
             onClick={() => {
               onDelete();
               onClose();
             }}
-            type="button"
-            className="px-4 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-400 focus:outline-none"
+            className="px-6 py-2 text-sm font-medium bg-orange-500 text-black rounded-xl hover:bg-orange-600"
           >
             Yes
           </button>
           <button
             onClick={onClose}
-            type="button"
-            className="px-4 py-2 text-white bg-gray-600 rounded-md hover:bg-gray-500 focus:outline-none"
+            className="px-6 py-2 text-sm font-medium bg-[#3B3737] text-orange-500 border border-orange-500 rounded-xl hover:bg-black"
           >
             No
           </button>
