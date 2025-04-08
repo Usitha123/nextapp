2528a6257899f880b93e81a15623229989aa6720
Usitha <103616801+Usitha123@users.noreply.github.com>
2025-04-08 08:03:24 +0530

    edit

--- app/UserView/page.jsx
+++ app/UserView/page.jsx
@@ -6,13 +6,13 @@ import Sidebar from './Sidebar/page';
 import Topbar from './Topbar/page';
 import Header from './Header/page';
 import { usePathname } from 'next/navigation';
-import ClearCookies from '@/components/ClearCookies';
+
 
 const Dashboard = () => {
   const currentPath = usePathname();
   return (
     <div className="flex bg-gray-100">
-      <ClearCookies />
+      
       <Sidebar activePath={currentPath} /> 
       <div className="flex-1">
         <Topbar />
