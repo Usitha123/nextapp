afce4cdead06c3138c31eb4fdb0083083f03119c
Mitharaf <mit79888@gmail.com>
2025-04-08 19:25:44 +0530

    profile page

--- app/Canteendashboard/Profile/Profile.jsx
+++ app/Canteendashboard/Profile/Profile.jsx
@@ -40,7 +40,7 @@ export default function Profile() {
   }
 
   return (
-    <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md mx-auto">
+    <div className="bg-[#2B2623] p-8 rounded-xl shadow-lg w-full max-w-md mx-auto">
 
   {/* Profile Icon */}
   <div className="relative flex items-center justify-center w-24 h-24 mx-auto text-orange-600 bg-orange-100 rounded-full">
@@ -51,35 +51,35 @@ export default function Profile() {
   {/* Profile Info */}
   <div className="text-sm space-y-4 mt-6">
     <div className="grid grid-cols-2 gap-4">
-      <label className="text-gray-600">First Name</label>
-      <span className="text-right">{targetStudent.firstName || "N/A"}</span>
+      <label className="text-orange-500">First Name</label>
+      <span className="text-right text-gray-200">{targetStudent.firstName || "N/A"}</span>
     </div>
-    <hr />
+    <hr className=" border-gray-500" />
     <div className="grid grid-cols-2 gap-4">
-      <label className="text-gray-600">Last Name</label>
-      <span className="text-right">{targetStudent.lastName || "N/A"}</span>
+      <label className="text-orange-500">Last Name</label>
+      <span className="text-right text-gray-200">{targetStudent.lastName || "N/A"}</span>
     </div>
-    <hr />
+    <hr className=" border-gray-500" />
     <div className="grid grid-cols-2 gap-4">
-      <label className="text-gray-600">Phone</label>
-      <span className="text-right">{targetStudent.phoneNumber || "N/A"}</span>
+      <label className="text-orange-500">Phone</label>
+      <span className="text-right text-gray-200">{targetStudent.phoneNumber || "N/A"}</span>
     </div>
-    <hr />
+    <hr className=" border-gray-500"/>
     <div className="grid grid-cols-2 gap-4">
-      <label className="text-gray-600">Email</label>
-      <span className="text-right">{targetStudent.email || "N/A"}</span>
+      <label className="text-orange-500 ">Email</label>
+      <span className="text-right text-gray-200">{targetStudent.email || "N/A"}</span>
     </div>
-    <hr />
+    <hr className=" border-gray-500"/>
     <div className="grid grid-cols-2 gap-4">
-      <label className="text-gray-600">NIC Number</label>
-      <span className="text-right">{targetStudent.nicNumber || "N/A"}</span>
+      <label className="text-orange-500">NIC Number</label>
+      <span className="text-right text-gray-200">{targetStudent.nicNumber || "N/A"}</span>
     </div>
 
     {/* Edit Button */}
     <div className="flex">
       <Link
         href={`/Canteendashboard/Profile/Edit`}
-        className="font-bold mx-auto mt-4 px-6 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-400 focus:outline-none"
+        className="px-4 py-2 mx-auto text-sm font-medium bg-[#3B3737] text-orange-500 border border-orange-500 rounded-xl hover:bg-black transition"
       >
         Edit
       </Link>
