b579a99c723c7fabe19836eeae2d1f7d54f4be9d
Mitharaf <mit79888@gmail.com>
2025-04-08 19:04:02 +0530

    icons

--- app/Canteendashboard/Dashboard.jsx
+++ app/Canteendashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { CircleDollarSign, Import, OctagonAlert, Package, RefreshCcw, Tag, Upload } from "lucide-react";
 import React from "react";
 
 // Reusable card component styled like the canteen/student cards
@@ -9,8 +10,8 @@ const DashboardCard = ({ title, value, icon }) => {
     <div className="relative flex flex-col h-32 w-60 items-center justify-center p-2 text-orange-500 bg-[#2B2623] rounded-lg">
       <div className={`flex items-center gap-2 ${valueBgColor} px-8 py-4 rounded-lg font-thin relative`}>
         <span>{String(value)}</span>
-        <span className="absolute top-4 right-4 text-xl">{icon}</span>
       </div>
+      <span className="absolute top-4 w-[18px] h-[20px] right-5 text-[2px]">{icon}</span>
       <div className="mt-2 text-gray-300 text-md">{title}</div>
     </div>
   );
@@ -18,16 +19,16 @@ const DashboardCard = ({ title, value, icon }) => {
 
 const DashboardCards = () => {
   const cards = [
-    { title: "New Orders", value: 3, icon: "🔄" },
-    { title: "Orders Ready", value: 5, icon: "📦" },
-    { title: "Orders Cancelled", value: 2, icon: "⚙️" },
-    { title: "Total Meals", value: 6, icon: "🏷️" },
-    { title: "Total Orders", value: 10, icon: "📈" },
-    { title: "Total Revenue", value: "12000 Rs", icon: "💰" },
+    { title: "New Orders", value: 3, icon: <Import/> },
+    { title: "Orders Ready", value: 5, icon: <Upload/>},
+    { title: "Orders Cancelled", value: 2, icon: <OctagonAlert/> },
+    { title: "Total Meals", value: 6, icon: <Tag/> },
+    { title: "Total Orders", value: 10, icon: <Package/> },
+    { title: "Total Revenue", value: "12000 Rs", icon: <CircleDollarSign/> },
   ];
 
   return (
-    <div className="flex flex-wrap justify-center gap-6 p-4 mt-4 rounded-lg xl:gap-16">
+    <div className="flex flex-wrap justify-center gap-6 p-4 rounded-lg xl:gap-16">
       {cards.map((card, index) => (
         <DashboardCard
           key={index}
