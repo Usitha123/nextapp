449cb94b534f13c5142931e97ffd5ece9c7d15c5
Usitha <103616801+Usitha123@users.noreply.github.com>
2025-04-19 11:53:46 +0530

    error fix

--- app/Canteendashboard/Header/page.jsx
+++ app/Canteendashboard/Header/page.jsx
@@ -1,14 +1,14 @@
-// Header/page.js
+// app/Canteendashboard/Header/page.jsx
 "use client";
 
 import React from 'react';
 
-const Header = ({ title }) => {
+const HeaderPage = () => {
   return (
     <header className="p-4 text-xl font-bold text-white bg-gray-600">
-      {title}
+      Canteen Header
     </header>
   );
 };
 
-export default Header;
+export default HeaderPage;
