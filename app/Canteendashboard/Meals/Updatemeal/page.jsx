29bf0b01e2f7a8bf4d33321464a0bf8ee602af5b
Mitharaf <mit79888@gmail.com>
2025-06-17 23:53:40 +0530

    canteen- orders

--- app/Canteendashboard/Meals/Updatemeal/page.jsx
+++ app/Canteendashboard/Meals/Updatemeal/page.jsx
@@ -12,7 +12,7 @@ const Meals = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex-1 bg-black">
+      <div className="flex-1 bg-bla">
         <Topbar />
         <Header title="Update Meals" /> {/* Pass another route-specific title */}
         <div className="p-4">
