662df56a447a1e6f4ae2b6a3b11b18381fb17be2
Mithara Fernandopulle <158183206+mitharaf@users.noreply.github.com>
2025-08-28 22:57:51 +0530

    Scroll fix in canteen dashboard

--- app/Canteendashboard/Profile/Edit/page.jsx
+++ app/Canteendashboard/Profile/Edit/page.jsx
@@ -6,9 +6,9 @@ import Profile from './Profile';
 
 function page() {
   return (
-    <div className="flex bg-black">
-      <Sidebar/>
-      <div className="ml-20 md:ml-60 h-[100vh] w-[100vw]"> 
+    <div className="flex bg-black min-h-screen">
+      <Sidebar />
+      <div className="ml-20 md:ml-60 flex-1 flex flex-col bg-black"> 
         <Topbar />
         <Header title="Profile" />
         <div className="p-4">
